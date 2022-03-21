a94d6fff317c67a923d93d87d626121419b452d6
Yeakob <yeakob677@gmail.com>
2022-03-21 22:10:33 +0600

    my-first

---
+++ src/Classtime.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+export default class Classtime extends Component {
+  
+  render() {
+  
+    return (
+      <div>
+        <h1>The next class will be</h1>
+        <p>Date :-</p>
+        <p>Time :-</p>
+        <p>Topic :-</p>
+      </div>
+    )
+  }
+}
+
